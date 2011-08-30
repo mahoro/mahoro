80e9bb071df63eab7d0b39fd0bff95c1e5610366
jcastagna <jeremie.castagna@gmail.com>
2011-08-29 22:38:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mahoro::Application.config.secret_token = '98118d8148067a8ce8e49a769713fa0473dcc08825972a53095c30d6f331c8e2b50c375b4e8ed127ac062b4bbaaf50a5cba1b8b35ab5fc37111a399d9bd5d703'
