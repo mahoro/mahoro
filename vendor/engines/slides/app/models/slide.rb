65f5f089c680c8998db136c7c894e24ecdbe0a52
jcastagna <jeremie.castagna@gmail.com>
2011-10-15 15:10:21 -0700

    updated slides to not be a page

--- vendor/engines/slides/app/models/slide.rb
+++ vendor/engines/slides/app/models/slide.rb
@@ -3,6 +3,6 @@ class Slide < ActiveRecord::Base
   acts_as_indexed :fields => [:title, :link]
 
   validates :title, :presence => true, :uniqueness => true
-  
+
   belongs_to :image
 end
--- vendor/engines/slides/db/migrate/create_slides.rb
+++ vendor/engines/slides/db/migrate/create_slides.rb
@@ -13,6 +13,9 @@ class CreateSlides < ActiveRecord::Migration
     add_index :slides, :id
 
     load(Rails.root.join('db', 'seeds', 'slides.rb'))
+
+    slide_page = ::Page.find(:first, :conditions => {:link_url => "/slides"})
+    slide_page.destroy! if slide_page
   end
 
   def self.down
